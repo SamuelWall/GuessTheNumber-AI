4dbdeff109595b0dac9ac57ad8bbb0d7bc30ca1d
SamuelWall <ywolnerman@gmail.com>
2020-07-08 16:43:15 -0400

    added image to show scaled version of number drawn

--- numReader.py
+++ numReader.py
@@ -18,6 +18,7 @@ import numpy as np
 
 # creating a 600 x 600 pixels canvas for mouse drawing
 canvas = np.ones((600,600), dtype="uint8") * 255
+canvas2 = np.zeros((280,280), dtype="uint8") * 255
 # designating a 400 x 400 pixels point of interest on which digits will be drawn
 canvas[100:500,100:500] = 0
 
@@ -26,7 +27,7 @@ end_point = None
 is_drawing = False
 
 def draw_line(img,start_at,end_at):
-    cv2.line(img,start_at,end_at,255,15)
+    cv2.line(img,start_at,end_at,255,25)
 
 def on_mouse_events(event,x,y,flags,params):
     global start_point
@@ -46,11 +47,13 @@ def on_mouse_events(event,x,y,flags,params):
 
 
 cv2.namedWindow("Test Canvas")
+cv2.namedWindow("Image Canvas")
 cv2.setMouseCallback("Test Canvas", on_mouse_events)
 
 
 while(True):
     cv2.imshow("Test Canvas", canvas)
+    cv2.imshow("Image Canvas", canvas2)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
@@ -61,6 +64,13 @@ while(True):
         input = cv2.resize(image, (20,20))
         input = np.pad(input, pad_width=4, mode='constant', constant_values=0).reshape((28,28,1)).astype('float32') / 255
         arr = np.reshape(np.array([input]), (784, 1))
+
+        input = cv2.resize(input,(280,280))
+        for r in range(0,len(input)):
+            row = input[r]
+            for c in range(0,len(row)):
+                col = row[c]
+                cv2.line(canvas2, (c,r), (c,r), int(input[r][c]*255), 1)
         #print(arr)
         result = net.guessNum(arr)
 
